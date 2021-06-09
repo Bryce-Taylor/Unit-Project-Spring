a41dd172c9bf587a17b1c0399352f52ec581d680
Bryce-Taylor <72210125+Bryce-Taylor@users.noreply.github.com>
2021-06-09 08:52:12 -0500

    Pushing project

---
+++ src/main/java/com/example/Chart/Creator/ChartCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chart.Creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Chart/Creator/ChartCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chart.Creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
